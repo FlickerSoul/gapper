269540a588fd4c1bede905445786726d486a1b67
Heyuan Zeng <hi@universe.observer>
2023-10-29 01:23:33 +0100

    test: test param helpers

--- tests/test_test_param.py
+++ tests/test_test_param.py
@@ -19,3 +19,21 @@ def test_test_params_gap_kwargs_length_matching() -> None:
         @problem()
         def square(a: int) -> int:
             return a**2
+
+
+def test_test_params_params() -> None:
+    @test_cases.params([1], [2], [3])
+    @problem()
+    def square(a: int) -> int:
+        return a**2
+
+    assert len(square.test_cases) == 3
+
+
+def test_test_params_param_iter() -> None:
+    @test_cases.param_iter([i] for i in range(3))
+    @problem()
+    def square(a: int) -> int:
+        return a**2
+
+    assert len(square.test_cases) == 3
