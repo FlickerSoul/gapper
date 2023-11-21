037d025dc1c1a49108407935538e8d23ca98b36b
Heyuan Zeng <hi@universe.observer>
2023-11-21 14:03:24 +0000

    fix: testing

--- tests/assets/problems/assess_post_tests.py
+++ tests/assets/problems/assess_post_tests.py
@@ -12,16 +12,13 @@ def check_at_least_half_is_correct(
     test_proxy: TestResult,
     metadata: GradescopeSubmissionMetadata | None,
 ) -> None:
-    test_proxy.set_max_score(0)
     # if the number of passed tests is greater than half of the total number of tests
     if sum(result.is_passed for result in test_results) > len(test_results) // 2:
         test_proxy.set_pass_status("passed")
 
-        test_proxy.set_extra_points(5)
-
 
 @test_cases.singular_param_iter([i for i in range(10)], gap_max_score=1)
-@post_tests(check_at_least_half_is_correct)
+@post_tests(check_at_least_half_is_correct, gap_max_score=0, gap_extra_points=5)
 @problem()
 def square(x: int | float) -> int | float:
     return x**2
