06259810f5a635f40d0dd183557b4bed5d12bc9c
Heyuan Zeng <hi@universe.observer>
2023-12-11 21:48:01 +0000

    fix: python executable linking

--- tests/test_zip_file_handle.py
+++ tests/test_zip_file_handle.py
@@ -25,10 +25,13 @@ def test_zipping(tmp_path: Path) -> None:
         set -euo pipefail
 
         # install python {major}.{minor}
+        apt-get remove -y python3
+        apt-get autoremove -y
         apt-get update -y
         apt-get install -y software-properties-common
         add-apt-repository -y ppa:deadsnakes/ppa
         apt-get install -y python{major}.{minor} python{major}.{minor}-distutils
+        ln -s -f $(which python3.12) $(which python3)
 
         # install gapper
         curl -sS https://bootstrap.pypa.io/get-pip.py | python{major}.{minor}
